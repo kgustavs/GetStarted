1fc6bc0b7efbb6571db499618e8d376cc918d27f
kgustavs <kjell.gustavsen@soprasteria.com>
2016-06-02 13:09:49 +0200

    add graddle files

--- src/main/java/com/getstarted/smallsurface/FirstApp.java
+++ src/main/java/com/getstarted/smallsurface/FirstApp.java
@@ -4,4 +4,7 @@ package com.getstarted.smallsurface;
  * Created by kgustavs on 02.06.2016.
  */
 public class FirstApp {
+    public static void main(String[] args){
+        System.out.println("This is my first app!");
+    }
 }
